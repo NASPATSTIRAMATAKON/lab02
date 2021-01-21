16a3e9b4bc69d0a48b88aa414a6240382be70d99
DESKTOP-2AH228N\KeeRong <614234012@parichat.skru.ac.th>
2021-01-21 09:11:52 +0700

    Updatelap02

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <div id="app">
+     {{ counter }}<br/>
+     <button @click="counter=counter+1">Click1</button><br/>
+     <button @click="clicked">Click2</button>
+
      <h1>{{ message }}</h1><br/>
-     <span v-text="message"></span>
+     <span v-text="message"></span><br/>
+
+     <input type="text" v-model="message" />
      
      <hr/>
      <h1 v-show="isShow">Hello!</h1>
@@ -21,6 +27,15 @@
      <a v-bind:href="urlSKRU">SKRU</a>
      <hr/>
      <img v-bind:src="require('@/assets/images/' + pictureName + '.jpg')" width="200px" />
+     <hr/>
+     <select v-model="message">
+       <option value="Bread">Bread</option>
+       <option value="Bts">Bts</option>
+       <option value="Blackpink">Blackpink</option>
+     </select>
+     <hr/>
+     <textarea v-model="message"></textarea>
+
   </div>
 </template>
 
@@ -32,6 +47,7 @@ export default {
   },
   data(){
     return{
+      counter:0,
       pictureName : 'bts',
       urlSKRU:"http://www.skru.ac.th",
       message:"Hello World!",
@@ -41,6 +57,11 @@ export default {
         {"id":2,"name":"Trump","Salary": 10000 } ,
         {"id":3,"name":"Peter","Salary": 99000  } , 
       ],
+    }
+  },
+  methods:{
+    clicked: function(){
+      this.counter=this.counter+2;
     }
   },
 }
